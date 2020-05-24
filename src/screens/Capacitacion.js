6ea3d7d12253ae5fa4603e2788c93445247acdfc
Andres Sanchez <andres_sa.97@hotmail.com>
2020-05-24 03:05:58 -0500

    Minor fixes, added final coordinador fields

--- src/screens/Capacitacion.js
+++ src/screens/Capacitacion.js
@@ -7,16 +7,19 @@ export default (props)=>{
 	var [data, setData] = useState(false);
 	var [refreshing, setRefreshing] = useState(false);
 	var [error, setError] = useState(false);
+	var [user, setUser] = useState(false);
 
 	useEffect(()=>{
-		API.getParroquias().then(d=>{
-			setData(d);
-			setRefreshing(false);
-			setError(false);
-		}).catch(err=>{
-			setRefreshing(false);
-			setError(true);
-		})
+		API.getUser().then(setUser);
+		// API.getParroquias().then(d=>{
+		// 	setData(d);
+		// 	setRefreshing(false);
+		// 	setError(false);
+		// }).catch(err=>{
+		// 	setRefreshing(false);
+		// 	setError(true);
+		// })
+		setData([])
 	}, [])
 
 	var getCapacitacion = ()=>{
@@ -48,8 +51,14 @@ export default (props)=>{
 			</View>
 		) : (
 			<View>
-				<Button text="Agregar Capacitacion" style={{ width: 250, alignSelf: 'center' }} onPress={addCapacitacion} />
-				<AlphabetList data={['a','b', 'c']} onSelect={onPress} scroll sort={'nombre'} />
+				{ user && (user.type=='admin' || user.type=='superadmin') && <Button text="Agregar Capacitacion" style={{ width: 250, alignSelf: 'center' }} onPress={addCapacitacion} /> }
+				{ (data.length>0) ? (
+					<AlphabetList data={data} onSelect={onPress} scroll headers={false}/>
+				) : (
+					<View>
+						<Text style={{ textAlign: 'center', fontSize: 16, color: 'gray', backgroundColor: 'white', padding: 15 }}>No perteneces a una capacitación.</Text>
+					</View>
+				)}
 			</View>
 		)}
 	</ScrollView>
--- src/screens/RegistroCoordinador.js
+++ src/screens/RegistroCoordinador.js
@@ -1,85 +1,164 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import { Input, Button, Picker } from '../components'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { API } from '../lib';
-import {Image} from 'react-native' ; 
+import { API, Util } from '../lib';
+import DatePicker from 'react-native-datepicker';
+import moment from 'moment/min/moment-with-locales'
+moment.locale('es')
 
 export default (props)=>{
-
 	var [loading, setLoading] = useState(false);
-	var [email, setEmail] = useState('prueba@prubea.com');
-	var [password, setPassword] = useState('contrasnaprueba');
-	var [name, setName] = useState('pepe');
-	var [age, setAge] = useState ('80');
-	var [gender, setGender] = useState (false);
+	var [name, setName] = useState('');
+	var [apPaterno, setApPaterno] = useState('');
+	var [apMaterno, setApMaterno] = useState('');
+	var [email, setEmail] = useState('');
+	var [birthday, setBirthday] = useState (moment().format('YYYY-MM-DD'));
+	var [gender, setGender] = useState(false);
+	var [estadoCivil, setEstadoCivil] = useState(false);
+	var [domicilio, setDomicilio] = useState('');
+	var [colonia, setColonia] = useState('');
+	var [municipio, setMunicipio] = useState('');
+	var [phoneHome, setPhoneHome] = useState('');
+	var [phoneMobile, setPhoneMobile] = useState('');
+	var [phoneMobile, setPhoneMobile] = useState('');
+	var [escolaridad, setEscolaridad] = useState(false);
+	var [oficio, setOficio] = useState(false);
+	var [password, setPassword] = useState('');
+	var pickerRef = useRef(null);
 
 	var onAdd = props.route.params.onAdd;
 
 	props.navigation.setOptions({
-		headerTitle: 'Registro Coordinador'
+		headerTitle: 'Registro Miembro'
 	});
 
 	var doRegister = ()=>{
-
-	// 	var payload = {
-	// 		nombre,
-	// 		apellido_paterno,
-	// 		apellido_materno,
-	// 		fecha_nacimiento, // YYYY-MM-DD, convertir a Date
-	// 		estado_civil,
-	// 		sexo,
-	// 		domicilio,
-	// 		colonia,
-	// 		municipio,
-	// 		telefono_casa,
-	// 		telefono_movil,
-	// 		email,
-	// 		escolaridad,
-	// 		oficio,
-	// 		ocupacion
-	//   }
- 
-
 		if(loading) return;
-		if(name.length<1) return alert ('Por favor introduzca un nombre.');
-		if(age<2) return alert ('Por favor introduzca una edad valida');
-		if(!gender) return alert('Favor de seleccionar el sexo.');
-		if(email.length<5) return alert('Favor de ingresar un correo electrónico válido.');
-		if(password.length<3) return alert('Favor de ingresar una contraseña válida');
+
+		var data = {
+			nombre: name,
+			apellido_paterno: apPaterno,
+			apellido_materno: apMaterno,
+			fecha_nacimiento: birthday,
+			estado_civil: estadoCivil ? estadoCivil.value : null,
+			sexo: gender ? gender.value : null,
+			email: email,
+			password: password,
+			escolaridad: escolaridad ? escolaridad.value : null,
+			oficio: oficio ? oficio.value : null,
+			domicilio: {
+				domicilio: domicilio,
+				colonia: colonia,
+				municipio: municipio,
+				telefono_casa: phoneHome,
+				telefono_movil: phoneMobile,
+			}
+		}
+
+		var { valid, prompt } = Util.validateForm(data, {
+			nombre: { type: 'minLength', value: 3, prompt: 'Favor de introducir el nombre.' },
+			apellido_paterno: { type: 'empty', prompt: 'Favor de introducir el apelldio paterno.' },
+			fecha_nacimiento: { type: 'empty', prompt: 'Favor de introducir la fecha de nacimiento.' },
+			sexo: { type: 'empty', prompt: 'Favor de introducir el sexo.' },
+			estado_civil: { type: 'empty', prompt: 'Favor de introducir el estado civil.' },
+			escolaridad: { type: 'empty', prompt: 'Favor de introducir la escolaridad.' },
+			oficio: { type: 'empty', prompt: 'Favor de introducir el oficio.' },
+			password: { type: 'minLength', value: 5, prompt: 'Favor de introducir la contraseña, minimo 5 caracteres' }
+		});
+
+		if(!valid){
+			return Alert.alert('Error', prompt);
+		}
 
 		setLoading(true);
-		API.registerCoordinador(name, age, gender.value, email, password).then(new_coordinador=>{
+		API.registerCoordinador(data).then(new_member=>{
 			setLoading(false);
-			if(!new_coordinador) return alert("Hubo un error registrando el coordinador");
-			if(onAdd) onAdd(new_coordinador)
-			alert("Se ha agregado el coordinador");
+			if(!new_member) return Alert.alert('Error', "Hubo un error registrando el coordinador");
+			if(onAdd) onAdd(new_member)
+			Alert.alert('Exito', "Se ha agregado el coordinador.");
 			props.navigation.goBack();
 		}).catch(err=>{
-			if(err.code==1){
-				alert("Ya existe un miembro del sistema con ese correo electrónico.");
-			}else{
-				alert("Hubo un error registrando el coordinador");
-			}
+			console.log(err);
+			Alert.alert('Error', "Hubo un error registrando el coordinador");
 			setLoading(false);
 		})
+	}
+
+	var formatDate = a=>{
+		var f = moment(a, 'YYYY-MM-DD').format('MMMM DD, YYYY')
+		return f.charAt(0).toUpperCase() + f.substr(1);
 	}
 
 	return (
 		<KeyboardAwareScrollView style={styles.loginContainer} bounces={true}>
 			<Text style={styles.header}>Registrar Coordinador</Text> 
-			<Input name="Nombre completo" value={name} onChangeText={setName}/>
-			<Input name="Edad" value={age} onChangeText={setAge} keyboard={'number-pad'} />
-			<Picker name="Sexo" items={[
+			<Input name="Nombre" value={name} onChangeText={setName} required/>
+			<Input name="Apellido Paterno" value={apPaterno} onChangeText={setApPaterno} required/>
+			<Input name="Apellido Materno" value={apMaterno} onChangeText={setApMaterno}/>
+			<Input value={formatDate(birthday)} name={'Fecha de nacimiento'} required readonly onPress={()=>{
+				pickerRef.current.onPressDate()
+			}} />
+			<Picker name="Estado Civil" required items={[
+				{ label: 'Soltero', value: 'Soltero' },
+				{ label: 'Casado', value: 'Casado' },
+				{ label: 'Viudo', value: 'Viudo' },
+				{ label: 'Unión Libre', value: 'Unión Libre' },
+				{ label: 'Divorciado', value: 'Divorciado' },
+			]} onValueChange={setEstadoCivil} />
+			<Picker name="Sexo" required items={[
 				{ label: 'Masculino', value: 'Masculino' },
 				{ label: 'Femenino', value: 'Femenino' },
 				{ label: 'Sin especificar', value: 'Sin especificar' }
 			]} onValueChange={setGender} />
+			<Picker name="Grado escolaridad" required items={[
+				{ label: 'Ninguno', value: 'Ninguno' },
+				{ label: 'Primaria', value: 'Primaria' },
+				{ label: 'Secundaria', value: 'Secundaria' },
+				{ label: 'Técnica carrera', value: 'Técnica carrera' },
+				{ label: 'Maestría', value: 'Maestría' },
+				{ label: 'Doctorado', value: 'Doctorado' },
+			]} onValueChange={setEscolaridad} />
+			<Picker name="Oficio" required items={[
+				{ label: 'Ninguno', value: 'Ninguno' },
+				{ label: 'Plomero', value: 'Plomero' },
+				{ label: 'Electricista', value: 'Electricista' },
+				{ label: 'Carpintero', value: 'Carpintero' },
+				{ label: 'Albañil', value: 'Albañil' },
+				{ label: 'Pintor', value: 'Pintor' },
+				{ label: 'Mecánico', value: 'Mecánico' },
+				{ label: 'Músico', value: 'Músico' },
+				{ label: 'Chofer', value: 'Chofer' },
+			]} onValueChange={setOficio} />
 
-			<Text style={styles.subHeader}>Credenciales</Text> 
-			<Input name="Correo electrónico" value={email} onChangeText={setEmail} textContentType={'emailAddress'} keyboard={'email-address'} />
-			<Input name="Contraseña" style={{ marginTop: 10 }} value={password} onChangeText={setPassword} textContentType={'password'} password />
+			<Text style={styles.section}>Domicilio</Text> 
+			<Input name="Domicilio" value={domicilio} onChangeText={setDomicilio} />
+			<Input name="Colonia" value={colonia} onChangeText={setColonia} />
+			<Input name="Municipio" value={municipio} onChangeText={setMunicipio} />
+			<Input name="Teléfono Casa" value={phoneHome} onChangeText={setPhoneHome} />
+			<Input name="Teléfono Móvil" value={phoneMobile} onChangeText={setPhoneMobile} />
+
+			<Text style={styles.section}>Credenciales</Text> 
+			<Input name="Correo electrónico" required value={email} onChangeText={setEmail} textContentType={'emailAddress'} keyboard={'email-address'} />
+			<Input name="Contraseña" required style={{ marginTop: 10 }} value={password} onChangeText={setPassword} textContentType={'password'} password />
 			<Button text="Registrar" loading={loading} onPress={doRegister} />
+
+			<DatePicker
+				ref={pickerRef}
+				date={birthday}
+				mode="date"
+				format="YYYY-MM-DD"
+				confirmBtnText="Confirmar"
+				cancelBtnText="Cancelar"
+				customStyles={{
+					dateIcon: { display: 'none' },
+					dateInput: { display: 'none' }
+				}}
+				locale={'es'}
+				onDateChange={d=>{
+					setBirthday(d);
+				}}
+			/>
 		</KeyboardAwareScrollView>
 	)
 }
@@ -102,15 +181,21 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: '600',
 		textAlign: 'center',
-		marginBottom: 20,
+		marginBottom: 5,
 		marginTop: 20,
 	},
 	subHeader: {
 		fontSize: 20,
-		fontWeight: '600',
 		textAlign: 'center',
 		color: 'grey',
 		marginBottom: 20,
-		marginTop: 10,
+	},
+	section: {
+		fontSize: 20,
+		fontWeight: '600',
+		textAlign: 'center',
+		color: 'grey',
+		marginBottom: 10,
+		marginTop: 20,
 	}
 })
