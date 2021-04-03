f2933d5cc27351db64826a4a8467f72ca88633e5
Cesar Barraza <cbarrazaa1@gmail.com>
2021-04-02 22:23:35 -0600

    feat(chat-post-creation): finish chat post creation view

--- src/screens/chat/ChatChannelCreatePost.js
+++ src/screens/chat/ChatChannelCreatePost.js
@@ -1,14 +1,73 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
+import { Button } from '../../components';
+import ChatChannelCreatePostOptionRow from '../../components/chat/ChatChannelCreatePostOptionRow';
+import { NavigationProps } from '../../navigation/NavigationPropTypes';
 
-function ChatChannelCreatePost() {
-  return <View style={styles.root}></View>;
+function ChatChannelCreatePost({ navigation }) {
+  navigation.setOptions({
+    headerTitle: 'Crear Publicación',
+  });
+
+  const onCameraPress = () => {};
+  const onFilePress = () => {};
+  const onMessageCreatePress = () => {};
+
+  return (
+    <View style={styles.root}>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribir mensaje..."
+        multiline={true}
+      />
+
+      <View style={styles.separator} />
+
+      <ChatChannelCreatePostOptionRow
+        iconName="camera"
+        title="Agregar foto/video"
+        onPress={onCameraPress}
+      />
+      <ChatChannelCreatePostOptionRow
+        iconName="paperclip"
+        title="Agregar archivo"
+        onPress={onFilePress}
+        hasTopBorder={false}
+      />
+
+      <Button
+        style={styles.createButton}
+        text="Nuevo Mensaje"
+        onPress={onMessageCreatePress}
+      />
+    </View>
+  );
 }
+
+ChatChannelCreatePost.propTypes = {
+  ...NavigationProps,
+};
 
 const styles = StyleSheet.create({
   root: {
     backgroundColor: '#EDEDED',
     paddingTop: 10,
+    flex: 1,
+  },
+  input: {
+    backgroundColor: 'white',
+    height: 160,
+    padding: 12,
+    outlineWidth: 0,
+  },
+  separator: {
+    height: 12,
+    backgroundColor: '#EDEDED',
+  },
+  createButton: {
+    width: 250,
+    alignSelf: 'center',
+    backgroundColor: '#EDEDED',
   },
 });
 
