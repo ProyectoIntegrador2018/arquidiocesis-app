ae5d1f80aa9fe32f02d00cee256e2daca2ee5aa3
Cesar Barraza <cbarrazaa1@gmail.com>
2021-04-05 12:33:44 -0500

    feat(groups-get-all-route): fix get all route path

--- src/lib/apiV2/GroupsConvAPI.js
+++ src/lib/apiV2/GroupsConvAPI.js
@@ -15,7 +15,7 @@ const ROOT_URL = 'https://arquidiocesis-bda.herokuapp.com/api/groups';
  * @returns {Promise<AllGroupsResponse>}
  */
 async function all() {
-  return await get(ROOT_URL);
+  return await get(`${ROOT_URL}/get`);
 }
 
 /**
@@ -31,7 +31,7 @@ async function add(params) {
   const data = await post(`${ROOT_URL}/`, {
     group_name: name,
     group_roles: roles,
-    group: channels,
+    group_channels: channels,
   });
   return data;
 }
